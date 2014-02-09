247ceea01b40783eb6b76b9fe53a976e5aeab994
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-09 18:21:46 -0500

    Modified document- quoteed out the previous script to place an example of String and Boolean Variables to the script file.

--- Values_Variables/js/script.js
+++ Values_Variables/js/script.js
@@ -1,3 +1,15 @@
+//Variables of Different Types, Numbers, Booleans and Strings
+
+var age = 31; //Number Variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Jay";
+
+var phrase = "I don\'t know";
+
+
+/*
 //Defining Variables
 
 var age; 						//declaration
@@ -7,7 +19,7 @@ age = 31; 						//definition
 
 var yearOfBirth = 1970; 		//declaring and defining on the same line
 
-/*
+
 // JavaScript
 
 
