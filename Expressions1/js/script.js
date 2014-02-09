b09fab03212126b30ffbaf451532927fe2f0f688
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-09 18:46:53 -0500

    Modified document- quoted out initial expression to reflect new "age" reflection example in the .js file.

--- Expressions1/js/script.js
+++ Expressions1/js/script.js
@@ -1,6 +1,22 @@
+//Expressions - Age Example
+
+var yearBorn = 1974;
+var age = 2013 - yearBorn - 1;
+console.log(age);
+
+
+
+
+
+
+
+
+
+/*
 //Expressions
 
 var a = 2; //sets up our variable and defines it with 2
 var b;
 b = a + 3; //add 3 to a 
 console.log(a);
+*/
