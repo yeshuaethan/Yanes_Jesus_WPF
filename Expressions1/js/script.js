40d9170263502a41391ca0ebd592c52636646c8b
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-09 18:54:28 -0500

    Modified document- I quoted out original script to place an Arithmetic Operator example in the .js script.

--- Expressions1/js/script.js
+++ Expressions1/js/script.js
@@ -1,3 +1,12 @@
+//Expressions - Arithmetic Operations
+
+//area of the triangle is half of the width times the height
+var width = 8;
+var height = 7;
+var area = width * height/2;
+console.log(area);
+
+/*
 //Expressions - Age Example
 
 var yearBorn = 1974;
@@ -5,14 +14,6 @@ var age = 2013 - yearBorn - 1;
 console.log(age);
 
 
-
-
-
-
-
-
-
-/*
 //Expressions
 
 var a = 2; //sets up our variable and defines it with 2
