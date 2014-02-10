81a3ae47bee114d354e451b4a3bfddd06d03424d
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-10 11:42:58 -0500

    Modified original document by quoting out the first word problem and adding the next word problem script "Slice of Pie Part 1."

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -1,9 +1,18 @@
 //Jesus Yanes, 2/10/14 Expression Worksheet assigment.
 
-
+/*
 // Calculate Sparky's age in dog years.
 var humanYear = 5; //Sparky is 5 years in human.
 var dogYear = 7; //Dog years is seven times human years.
 var sparkyAge = humanYear*dogYear
 console.log(sparkyAge);
 // Sparky the dog is 56 years old.
+*/
+
+// Slice of Pie Part I
+var slicePerPizza = 8;
+var partygoer = 25;
+var pizzasOrdered = 5;
+var totalPizzaPerPerson = (slicePerPizza * pizzasOrdered) / partygoer;
+console.log(totalPizzaPerPerson)
+// Thee total amount of pizza that each partygoer had was 1.6
