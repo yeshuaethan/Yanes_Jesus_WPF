04ea21e9c7bdbb83d0d8fa12935320910eb97489
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-10 14:36:40 -0500

    Modified document by quoting out the previous word problems scripts and adding the last word problem script titled "Discounts."

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -16,7 +16,6 @@ var pizzasOrdered = 5;
 var totalPizzaPerPerson = (slicePerPizza * pizzasOrdered) / partygoer;
 console.log(totalPizzaPerPerson)
 // Thee total amount of pizza that each partygoer had was 1.6.
-*/
 
 
 // Slice of Pie Part II
@@ -27,3 +26,28 @@ var totalPizzaPerPerson = (slicePerPizza * pizzasOrdered) / partygoer;
 var sliceSparkyEats = (slicePerPizza * pizzasOrdered) % partygoer; 
 console.log(sliceSparkyEats)
 // slice sparky eats is 15.
+
+
+// Average Shopping Bill
+var weeklyExpenses = [95 , 185 , 120 , 250, 150]; // Array of 5 Weekly Shopping Expenses
+var total = weeklyExpenses [0] + weeklyExpenses [1] + weeklyExpenses [2] + weeklyExpenses [3] + weeklyExpenses [4]; 
+console.log(total);
+// Total Monthly Expenses in Grocery 800.
+var monthlyExpense = 800; // Array of the total 5 weeks expenses
+var totalWeekInaMonth = 5; // Divided by 5 weeks in a month
+var totalWeekPercentage = monthlyExpense / totalWeekInaMonth;
+console.log(totalWeekPercentage)
+// $160.00 per week.
+*/
+
+
+// Discounts
+var cellPhoneOriginalPrice = 600;  // Item Original Price $600.00
+var percentDiscount = 0.50; // Store Discount 50%
+var tax = 6.0; // Florida State Tax
+var cellPhoneAfterDiscount = cellPhoneOriginalPrice - (cellPhoneOriginalPrice * percentDiscount) // Item original price, minus original price, times discount. 
+var cellPhoneAfterDiscountwithTax = cellPhoneAfterDiscount * tax / 100
+// Cellphone after discount with no tax is $300.00
+var cellPhoneTotal = cellPhoneAfterDiscountwithTax + cellPhoneAfterDiscount;
+console.log(cellPhoneTotal)
+// Total of the cellphone after discount with tax is $318.00
