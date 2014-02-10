a3ba7f5b9aa7ce16cccb2a14e42db732c7a53126
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-09 22:10:10 -0500

    Created the initial files to include the index.html and the script.js file, for "Prompts and Variables" example script.

---
+++ Prompts_Alerts/js/script.js
@@ -0,0 +1,13 @@
+//Prompt and Alert
+/*
+var userImput = prompt("Enter your year of birth:");
+console.log(userImput);
+*/
+
+var width = prompt("We are calcultating the are of a rectangle. \n Please enter the width");
+var height = prompt("Please enter the height.");
+var area = width * height; //calculates with that info
+//console.log(area); //prints it out to console
+
+var result = "The area of the triangle is " + area + " sq feet";
+alert(result);
