515e40bf2cec878041bed70d6ca986839c479d38
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-10 10:57:21 -0500

    Modified original blank document to include my name, date, assignment name, and the first word problem script (Sparky's Age in dog years).

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -1,1 +1,9 @@
 //Jesus Yanes, 2/10/14 Expression Worksheet assigment.
+
+
+// Calculate Sparky's age in dog years.
+var humanYear = 5; //Sparky is 5 years in human.
+var dogYear = 7; //Dog years is seven times human years.
+var sparkyAge = humanYear*dogYear
+console.log(sparkyAge);
+// Sparky the dog is 56 years old.
