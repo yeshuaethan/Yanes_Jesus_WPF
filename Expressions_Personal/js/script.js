d27b185d3bf446ea897b3ce98ba5d71e572c87e9
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-12 20:37:30 -0500

    Modified document to include the first part of my script example.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,3 +6,8 @@
 //I need to find out the amount of protein powder needed on a monthly basis.
 //I will make a calculator for fitness enthusiast that calculates the amount of protein powder needed on a monthly basis.
 //This can probably be used to calculate any brand of protein powder out there.
+
+//Example:
+//I will tell you how much protein powder you consume in cups, per months you put in. (You will have convert cups to pounds yourself.)
+//Taking into account that there are 4 weeks in a month and also the amount of days in a week that you workout (Example: 5).
+//How much protein powder do I need every months?
