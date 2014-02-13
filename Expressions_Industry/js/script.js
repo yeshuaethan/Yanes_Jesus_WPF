09151e7ade78580309f8bb275874a19851dae85b
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-12 21:27:37 -0500

    Modified document- wrote my final comments on the script.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,3 +5,5 @@
 //I currently work as an Implementations Engineer. I work on Web Deployments, application upgrades, and code migrations amongst other things.
 //Assuming that you can get all the Web Deployments you need done in 2 hrs.
 //Typical job has 9-5 work time, 40 hr week work time. (Say there's 4 weeks in a month)
+//You take a total of an hour break a day, which sums to 5 hrs a week.
+//How many Web Deployments can be finished in a month?
