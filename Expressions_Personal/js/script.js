7d5d3e82e4de549457ea5a1e9d40b9683df77f29
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-12 20:33:53 -0500

    Modified document- I continued placing my comments on what the script is going to be about.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,3 +4,5 @@
 
 //Since I'm a gym freak and solely depend on protein shakes, I decided to do a protein powder calculator.
 //I need to find out the amount of protein powder needed on a monthly basis.
+//I will make a calculator for fitness enthusiast that calculates the amount of protein powder needed on a monthly basis.
+//This can probably be used to calculate any brand of protein powder out there.
