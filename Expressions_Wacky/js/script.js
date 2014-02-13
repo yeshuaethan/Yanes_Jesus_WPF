726c5d9052da5bc8c7f5669e866a30ee5bf814e6
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-13 10:42:54 -0500

    Modified document to begin with comments of what this script is all about.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,7 @@
 // Jesus Yanes-Fernandez
 // February 13, 2014	
 // Assignment: Expressions - Expressions_Wacky
+
+// Not to wacky, if you've noticed I'm a fitness freak. So, I decided to make a bodyfat percent calculator
+// The formulas below are just approximations. 
+// The goal here is to have a point of reference from which to work. 
