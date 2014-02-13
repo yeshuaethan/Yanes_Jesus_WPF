429389e93dd6ca65cec47beb2d1fb9a8ae9c60e1
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-12 21:21:53 -0500

    Modified document- began placing my comments on the script.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,3 +2,4 @@
 //February 12, 2014	
 //Assignment: Expressions - Expressions_Industry
 
+//I currently work as an Implementations Engineer. I work on Web Deployments, application upgrades, and code migrations amongst other things.
