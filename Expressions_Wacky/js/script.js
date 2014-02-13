2014557cf37cfe33cdc36a6eb5cab07bd10278df
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-13 10:43:40 -0500

    Modified document to include the first part of my bodyfat calculator script.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,3 +5,10 @@
 // Not to wacky, if you've noticed I'm a fitness freak. So, I decided to make a bodyfat percent calculator
 // The formulas below are just approximations. 
 // The goal here is to have a point of reference from which to work. 
+
+var first = 125 * 0.732;
+var second = 91.5 + 8.987;
+var third = 6 / 3.14;
+var fourth = 24 * 0.157;
+var fifth = 38 * 0.249;
+var sixth = 9.5 * 0.434;
