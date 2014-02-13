4b829038c95d318f94937e353248213bd6bc58bb
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-12 21:05:32 -0500

    Modified document to include my comments under var results.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -35,7 +35,7 @@ var amount = prompt("How many scoops of protein powder do you add to your shake(
 
 var first = dailyConsumption * amount; //Depending on your input for daily consumpion and amount of protein scoops you use daily, this will equal you total daily scoops consuption.
 var second = (first * workoutDayWeek) * weekMonth; // This line will take your total amount of daily consuption scoops, the amount of times you workout in a week, and multiply it against the total amount of weeks in a month (4).
-var result = second * months;
+var result = second * months; //Finally this will take your total daily scoop consuption, plus the amount of workout days in a week, times the 4 weeks in a month; to finally calculate that total against the total months you choose to get results for.
 
 var show = "The amount of protein powder you consume in a month(s) is:" + " " + result + " " + "cups";
 alert(show);
