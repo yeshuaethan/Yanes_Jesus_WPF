fae91fbf358c10a87163badb7a5e42a5e469ea2d
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-12 21:30:51 -0500

    Modified document to include my example script.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,8 +2,18 @@
 //February 12, 2014	
 //Assignment: Expressions - Expressions_Industry
 
+//Example in comment section below:
+
 //I currently work as an Implementations Engineer. I work on Web Deployments, application upgrades, and code migrations amongst other things.
 //Assuming that you can get all the Web Deployments you need done in 2 hrs.
 //Typical job has 9-5 work time, 40 hr week work time. (Say there's 4 weeks in a month)
 //You take a total of an hour break a day, which sums to 5 hrs a week.
 //How many Web Deployments can be finished in a month?
+
+var month = 1;
+var workTime = 40;
+var breakTime = 5;
+var webDeploymentPer = (40 - 5)/2;
+var result = webDeploymentPer * 4;
+
+console.log(result);
