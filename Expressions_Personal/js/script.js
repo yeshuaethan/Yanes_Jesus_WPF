c0193fb1d1b228b529f340f746cc452ac52c60e7
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-12 20:32:30 -0500

    Modified document- I began placing my comments on what the script is going to be about.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,6 @@
 //Jesus Yanes-Fernandez
 //February 11, 2014	
 //Assignment: Expressions - Expressions_Personal
+
+//Since I'm a gym freak and solely depend on protein shakes, I decided to do a protein powder calculator.
+//I need to find out the amount of protein powder needed on a monthly basis.
