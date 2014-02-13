c60213208096970446781564ed110760c7265f7e
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-12 21:26:11 -0500

    Modified document- continued to add my comments on the script.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,3 +3,5 @@
 //Assignment: Expressions - Expressions_Industry
 
 //I currently work as an Implementations Engineer. I work on Web Deployments, application upgrades, and code migrations amongst other things.
+//Assuming that you can get all the Web Deployments you need done in 2 hrs.
+//Typical job has 9-5 work time, 40 hr week work time. (Say there's 4 weeks in a month)
