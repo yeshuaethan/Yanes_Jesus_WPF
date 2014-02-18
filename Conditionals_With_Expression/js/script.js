57a861b8c898bd8af8ed1157291c43f854418eae
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-17 20:53:50 -0500

    Created all required documents to comply with this assignment, to include comparing the values of two literal values, or two variables, you can also evaluate the result of an expression.

---
+++ Conditionals_With_Expression/js/script.js
@@ -0,0 +1,14 @@
+// Conditional Logic - With an Expression
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the roller coaster!");
+}else{
+    console.log("You cannot ride the roller coaster!");
+}
