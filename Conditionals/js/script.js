d23326dda635543ae07b7d248ae6d314aa444607
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-17 20:49:28 -0500

    Created document to include all documents (.js, .html) and all sub folders needed to complete this assignment, to include the basic structure of a conditional statement.

---
+++ Conditionals/js/script.js
@@ -0,0 +1,12 @@
+// Conditional Logic
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!"
+
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the roller coaster!");
+}else{
+    console.log("You cannot ride the roller coaster!");
+}
