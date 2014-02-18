bf7471e42fae7e31a18961e796d1aa93e046a048
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-17 20:52:39 -0500

    Created all required documents to complete this assignment, to include comparing values with relational expressions is essential for decision making in programming.

---
+++ Conditionals_Relational_Expressions/js/script.js
@@ -0,0 +1,13 @@
+// Conditional Logic
+
+var kidHeight = 50;
+var minHeight = 38;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the roller coaster!");
+}else{
+    console.log("You cannot ride the roller coaster!");
+}
