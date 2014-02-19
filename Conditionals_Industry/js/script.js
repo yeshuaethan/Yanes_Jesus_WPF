f77b2074de1b8fab3017386996e36f0379fd25eb
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-19 14:35:41 -0500

    Modified document to include the first variables to the script.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,3 +4,9 @@
 
 // Assisting with pricing in web design
 // Lets get all the information we need to get started including hourly wage, minimum quote, project name, time and costs.
+
+var hourlyWage
+var minimumQuote
+var projectName
+var estimatedTime
+var costs
