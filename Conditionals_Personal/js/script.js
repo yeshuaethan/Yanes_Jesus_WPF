215f6e4925df0055a2877a6c3c74f253d537da8e
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-19 15:14:12 -0500

    Modified document to include prompt question

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,6 +2,6 @@
 // February 17, 2014
 // Conditionals Personal Script
 
-var username = prompt
+var username = prompt("Hello there, please enter your name.");
 var distance = prompt
 var speed = prompt
