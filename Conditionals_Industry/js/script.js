66e6451ef44546fb5572e6d4f8946ceffb16aac0
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-19 14:36:28 -0500

    Modified document to include the = prompt portion of the variable.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,8 +5,8 @@
 // Assisting with pricing in web design
 // Lets get all the information we need to get started including hourly wage, minimum quote, project name, time and costs.
 
-var hourlyWage
-var minimumQuote
-var projectName
-var estimatedTime
-var costs
+var hourlyWage = prompt
+var minimumQuote = prompt
+var projectName = prompt
+var estimatedTime = prompt
+var costs = prompt
