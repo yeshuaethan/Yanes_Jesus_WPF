dbde5a57dd6e093c3e32f8d5cd6cce0717976cad
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-19 14:39:17 -0500

    Modified document to include prompt questions.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,8 +5,8 @@
 // Assisting with pricing in web design
 // Lets get all the information we need to get started including hourly wage, minimum quote, project name, time and costs.
 
-var hourlyWage = prompt
-var minimumQuote = prompt
+var hourlyWage = prompt("How much would you like to get payed per hour?"); 
+var minimumQuote = prompt("What is the minimum quote you will allow to start the project?");
 var projectName = prompt
 var estimatedTime = prompt
 var costs = prompt
