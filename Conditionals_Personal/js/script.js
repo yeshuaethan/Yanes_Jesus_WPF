27cbd9ad9a31eea5dbf69898ef0d2928d2f03a51
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-19 15:14:32 -0500

    Modified document to include prompt question

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,5 +3,5 @@
 // Conditionals Personal Script
 
 var username = prompt("Hello there, please enter your name.");
-var distance = prompt
+var distance = prompt("Hello " + username + ". How many miles are your driving to work today?");
 var speed = prompt
