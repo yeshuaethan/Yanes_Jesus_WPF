0e2b016888ebfec292e396b881a750fdf05f4b05
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-20 10:48:11 -0500

    Modified document to include the console.log

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,3 +6,4 @@ confirm = prompt ("Hi there, random question- Did you have coffee today?");
 console.log("Fascinating, I didn't, Can you guess why?");
 
 var coffee = prompt ("Why do you think I didn't have coffee");
+console.log("I apologize, I didn't hear you. Something is byting me.");
