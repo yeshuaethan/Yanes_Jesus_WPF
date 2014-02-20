716856ceadf86a4fb24083570c69fb3808111629
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-20 10:47:44 -0500

    Modified document to include the question under variable coffee

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -5,4 +5,4 @@
 confirm = prompt ("Hi there, random question- Did you have coffee today?");
 console.log("Fascinating, I didn't, Can you guess why?");
 
-var coffee = prompt
+var coffee = prompt ("Why do you think I didn't have coffee");
