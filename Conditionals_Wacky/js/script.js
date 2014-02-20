564f7826d39f2a847fc58435bcf21af409b05f4d
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-20 10:48:35 -0500

    Modified document to include variable username

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -7,3 +7,5 @@ console.log("Fascinating, I didn't, Can you guess why?");
 
 var coffee = prompt ("Why do you think I didn't have coffee");
 console.log("I apologize, I didn't hear you. Something is byting me.");
+
+var username = prompt
