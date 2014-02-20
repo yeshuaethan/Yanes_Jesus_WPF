972ceca7334daa0fb6652d5c65521433dc3ea11d
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-20 10:51:52 -0500

    Began adding conditionals to my script

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -12,3 +12,10 @@ var username = prompt ("What's your name?");
 console.log("Fascinating...");
 
 var reason = prompt ("What brings you to my domain? (Money, Ad, Million Dollars)");
+if (reason == "money" || reason == "ad"|| reason == "million dollars"){
+    
+    console.log("That sucks, the last guy who came by already claimed the money from the ad.")    
+
+} else {
+    console.log("Huh, I thought it was the million dollar ad I posted. I was going to give it to you. That's cool.");
+}
