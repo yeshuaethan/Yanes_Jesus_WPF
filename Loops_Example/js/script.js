877a152c254cf6ae9fc94c3c8cf35bd2b5d6a89a
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-24 11:56:28 -0500

    Loops example assignment. Created all documents and sub folders for this assignment

---
+++ Loops_Example/js/script.js
@@ -0,0 +1,26 @@
+//loops
+
+console.log('---- Loops -----');
+
+var b = 50; //sets up the index
+
+while (b > 0){ //checks the condition
+    console.log(b + ' kegs on the wall');
+    b--; //increments b++ or decrements b-- the index 
+}
+
+console.log('---Do While Loop---');
+
+var c = 10
+
+do{
+    console.log(c + ' kegs on the wall');
+    c--;
+}
+while(c > 0);
+
+console.log('---For Loops---');
+
+for (var i = 10; i > 0; i--){
+    console.log(i + ' kegs on the wall');
+    
