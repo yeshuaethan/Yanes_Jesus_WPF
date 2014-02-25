e3075eee628bc84ec24ab5bd087ab45a7b981336
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-25 12:45:03 -0500

    Modified document to include variables female and male

--- Functions_Personal/js/functions.js
+++ Functions_Personal/js/functions.js
@@ -5,3 +5,6 @@
 var gender = prompt ("Gender? \n Enter 1 for Female or 2 for Male"); // Ask user for Gender
 var weight = prompt ("Enter your weight in pounds"); // Ask user for weight in pounds
 var height = prompt ("Enter your height in inches"); // Ask user for height in inches
+
+var Female = 100.1 + (5.06 * (height - 60));
+var Male = 110 + (5.06 * (height - 60));
