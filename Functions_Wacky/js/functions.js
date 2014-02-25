8c594b75c867f6f1ffea8caeead76c0d24f19a47
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-25 11:44:54 -0500

    Modified document to include "if" statement for the script

--- Functions_Wacky/js/functions.js
+++ Functions_Wacky/js/functions.js
@@ -10,4 +10,13 @@ alert(message);
 var message = "We are going to find out how many skittles are needed to fill your entire room";
 alert(message);
 var color = prompt ("What color do you prefer more, red or blue?")// Finding the amount of skittles  to fill the entire area of a room!
+
 // If statement blue
+if(color == "blue"){
+
+var widthOfRoom = prompt ("How wide is the room?");
+
+var heightOfRoom =prompt ("How high is the room");
+
+var total = calcArea(widthOfRoom, heightOfRoom);
+}
