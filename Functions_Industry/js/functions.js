0b33291d3002f734a60e0fff400e288c36a9e9db
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-25 14:26:18 -0500

    Modified document to include the prompt questions

--- Functions_Industry/js/functions.js
+++ Functions_Industry/js/functions.js
@@ -4,7 +4,7 @@
 
 // Determine your Compound Interest on your Savings Account
 
-var initialValue = prompt
-var rate = prompt
-var numberOfYears = prompt
-var compYear = prompt
+var initialValue = prompt ("Initial amount you deposited in dollars");
+var rate = prompt ("Enter the annual rate of interest in decimals");
+var numberOfYears = prompt ("Enter the number of years the amount has been deposited");
+var compYear = prompt ("Enter the number of times the interest is compounded yearly \n 1 for yearly; 2 for half a yearly; 3 for quarterly; 4 for monthly")
