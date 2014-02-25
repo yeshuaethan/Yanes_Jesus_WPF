9e1d3e6019cca1ab8606419b2e5e566ddad82b03
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-25 12:52:17 -0500

    Modified document to include the prompt questions for all three variables

--- Functions_Personal/js/functions.js
+++ Functions_Personal/js/functions.js
@@ -4,6 +4,6 @@
 
 // This script calculate the completion time of a script that will send out bulk emails.
 
-var emails = prompt
-var scriptSpeed = prompt
-var emailCap = prompt
+var emails = prompt ("How many emails do you need to send out today?");
+var scriptSpeed = prompt ("How many emails does your script send out per minute?");
+var emailCap = prompt ("How many emails can your script send out per day?");
