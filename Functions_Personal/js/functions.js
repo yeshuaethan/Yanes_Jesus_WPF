674ef9fedc7a91f61894dc8b0a9e21e5ddcc3164
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-25 12:40:09 -0500

    Modified document to include the prompt for var gender

--- Functions_Personal/js/functions.js
+++ Functions_Personal/js/functions.js
@@ -2,6 +2,6 @@
 // February 25, 2014
 // Functions Personal Assignment
 
-var gender = prompt
+var gender = prompt ("Gender? \n Enter 1 for Female or 2 for Male");
 var weight = prompt
 var height = prompt
