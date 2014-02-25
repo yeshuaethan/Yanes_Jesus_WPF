ba1f4c033305fb1efdcb5fce187ec01d6ea28203
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-25 12:59:53 -0500

    Modified document to include variable time to send

--- Functions_Personal/js/functions.js
+++ Functions_Personal/js/functions.js
@@ -7,3 +7,5 @@
 var emails = prompt ("How many emails do you need to send out today?");
 var scriptSpeed = prompt ("How many emails does your script send out per minute?");
 var emailCap = prompt ("How many emails can your script send out per day?");
+
+var timeToSend = calculateTimeToSend(emails, scriptSpeed, emailCap);
