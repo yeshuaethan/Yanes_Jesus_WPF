ed9f184c61527c1393769a185162e427580f54d4
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-25 11:27:59 -0500

    Modified document to include first alert message

--- Functions_Wacky/js/functions.js
+++ Functions_Wacky/js/functions.js
@@ -3,3 +3,7 @@
 // Functions Wacky Assignment
 
 // We are going to find out how many skittles are needed to fill an entire room from top to bottom.
+
+// Ask what color the end user prefers
+var message = "Welcome to the Skittles Factory";
+alert(message);
