c53f3b478c5c1b9a663c21d518039b7ef3c7559a
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-25 12:43:42 -0500

    Modified document to include the comment sections for all 3 variables

--- Functions_Personal/js/functions.js
+++ Functions_Personal/js/functions.js
@@ -2,6 +2,6 @@
 // February 25, 2014
 // Functions Personal Assignment
 
-var gender = prompt ("Gender? \n Enter 1 for Female or 2 for Male");
-var weight = prompt ("Enter your weight in pounds");
-var height = prompt ("Enter your height in inches");
+var gender = prompt ("Gender? \n Enter 1 for Female or 2 for Male"); // Ask user for Gender
+var weight = prompt ("Enter your weight in pounds"); // Ask user for weight in pounds
+var height = prompt ("Enter your height in inches"); // Ask user for height in inches
