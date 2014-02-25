b0a823aafbf89702c1f75bb9bdaf9cdac83a3a36
Jesus Yanes <jesus.yanes@fullsail.edu>
2014-02-25 14:28:01 -0500

    Modified document to include my comments to initial varibales

--- Functions_Industry/js/functions.js
+++ Functions_Industry/js/functions.js
@@ -4,7 +4,7 @@
 
 // Determine your Compound Interest on your Savings Account
 
-var initialValue = prompt ("Initial amount you deposited in dollars");
-var rate = prompt ("Enter the annual rate of interest in decimals");
-var numberOfYears = prompt ("Enter the number of years the amount has been deposited");
-var compYear = prompt ("Enter the number of times the interest is compounded yearly \n 1 for yearly; 2 for half a yearly; 3 for quarterly; 4 for monthly")
+var initialValue = prompt ("Initial amount you deposited in dollars"); // Ask user for the initial amount deposited
+var rate = prompt ("Enter the annual rate of interest in decimals"); // Ask user for the annual interest rate
+var numberOfYears = prompt ("Enter the number of years the amount has been deposited"); // Ask user for the number of years the amount has been deposited
+var compYear = prompt ("Enter the number of times the interest is compounded yearly \n 1 for yearly; 2 for half a yearly; 3 for quarterly; 4 for monthly") // Ask user how many times is the interest rate compounded yearly
